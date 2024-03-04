ccfe13aa96642925d46003e87ba73600637a5b31
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-03-04 20:06:51 +0900

    Updated: Example file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'show_location/show_location.dart';
+import 'package:webview_location/webview_location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,5 +14,45 @@ class MyApp extends StatelessWidget {
       title: 'Webview Location Example',
       home: ShowLocation(),
     );
+  }
+}
+
+class ShowLocation extends StatelessWidget {
+  const ShowLocation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Webview Location Example'),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const ViewLocation(),
+            ),
+          );
+        },
+        child: const Text('Show Location in Webview'),
+      )),
+    );
+  }
+}
+
+class ViewLocation extends StatelessWidget {
+  const ViewLocation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('View Location'),
+        ),
+        body: const ShowLocationInWebview(
+          locationName: 'Yonago, Tottori, Japan',
+        ));
   }
 }
